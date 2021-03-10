901f700ccce69896d5a60e406e1944537571d7f2
HitLyn <sc_lyn@outlook.com>
2021-03-10 17:07:38 +0100

    vae model

--- vae/vae.py
+++ vae/vae.py
@@ -9,5 +9,44 @@ from torch.utils.data.sampler import BatchSampler
 def image_loader(path):
     return Image.open(path).convert('RGB')
 
+class VaeImageDataset(Dataset):
+    def __init__(self, base_path, n_samples = 5e3, train = True, split = False, transform = None, loader = image_loader):
+        """
+            base_path: path/to/generated_images, which can get the full path: path/to/generated_images
+            n_samples: how many samples are there in the training set
+            train: bool, is this dataset for training or evaluation
+            loader: function, how to load the images
+        """
+        self.n_samples = n_samples
+        self.base_path = base_path
+        self.transform = transform
+        self.train = train
+        self.transform = transform
+        self.loader = image_loader
+        self.split = split
 
+    def __getitem__(self, index):
+        if self.split:
+            # get image starting idx
+            if self.train:
+                starting_idx = 1
+            else:
+                starting_idx = int(0.8*self.n_samples + 1)
+        else:
+            starting_idx = 1
+
+        file_name = "{:05d}.png".format(index + starting_idx)
+        image = self.loader(os.path.join(self.base_path, file_name))
+        if self.transform is not None:
+            image = self.transform(image)
+        return image 
+
+    def __len__(self):
+        if self.split:
+            if self.train:
+                return int(0.8*self.n_samples)
+            else:
+                return int(0.2*self.n_samples)
+        else:
+            return int(self.n_samples)
 
