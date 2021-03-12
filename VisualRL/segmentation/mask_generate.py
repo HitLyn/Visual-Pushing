fb5e523f1218f1de511320265d76b628f13e56d8
lyn <sc_lyn@outlook.com>
2021-03-12 20:14:27 +0100

    mask generation file

--- VisualRL/segmentation/mask_generate.py
+++ VisualRL/segmentation/mask_generate.py
@@ -4,6 +4,21 @@ import os
 import numpy as np
 
 IMAGE_PATH = "/homeL/cong/dataset/"
+SAVE_PATH = "/homeL/cong/dataset/"
+
+for i in range(1000):
+    file_name = os.path.join(IMAGE_PATH, "{:0>5d}.png".format(i))
+    image = cv2.imread(file_name)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+
+    light_red = (0, 150, 0)
+    bright_red = (20, 255, 255)
+    mask = cv2.inRange(image, light_red, bright_red)
+    crop_mask = mask[200:, 100:400]
+    crop_mask = cv2.resize(crop_mask, (64, 64))
+    save_name = os.path.join(SAVE_PATH, "{:0>5d}.png".format(i))
+    cv2.imwrite(save_name, crop_mask)
 
 
 
