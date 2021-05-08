b4d804f959972c8900093609cd101cf3149b8ed4
TAMS Lab <tams-lab@informatik.uni-hamburg.de>
2021-05-08 21:54:45 +0200

    update real file

--- VisualRL/real/visual_push.py
+++ VisualRL/real/visual_push.py
@@ -61,12 +61,14 @@ class VisualPushing:
     def __init__(self, device = None):
         self.step_ = 0
         self.goal_latent = None
+        self.goal_mask = None
         self.vel_pub = rospy.Publisher('/dynamic_pushing/velocity', geometry_msgs.msg.Vector3, queue_size=1)
         self.goal_publisher = rospy.Publisher('/pushing/goal_recon', Image, queue_size = 2)
         self.mask_publisher = rospy.Publisher('/pushing/mask', Image, queue_size=2)
         self.image_recon_publisher = rospy.Publisher('/pushing/mask_recon', Image, queue_size=2)
         self.image_crop_publisher = rospy.Publisher('/pushing/image_crop', Image, queue_size=2)
         self.image_crop_segmentation_publisher = rospy.Publisher('/pushing/image_crop_hsv', Image, queue_size=2)
+        self.overall_publisher = rospy.Publisher('/pushing/overall', Image, queue_size=2)
         # update pusher tf
         self.gripper_pos = np.zeros(3)
         rospy.Subscriber("/tf_pusher", Float64MultiArray, self.pusher_tf_update)
@@ -80,7 +82,7 @@ class VisualPushing:
         # goal pose initialization
         self.vae = VAE(device = self.device, image_channels = 1, h_dim = 1024, z_dim = 4)
         self.vae.load("/homeL/cong_pushing/HitLyn/Visual-Pushing/results/vae/04_30-13_51/vae_model", 100, map_location=self.device)  # latent space = 4
-        self.goal_latent = self.initialize_goal()
+        self.goal_latent, self.goal_mask = self.initialize_goal()
         # self.goal_latent = np.array([ 1.4946128,  0.6009053,  5.4465885, -1.370616 ])
 
 
@@ -103,8 +105,8 @@ class VisualPushing:
             raw_image = rospy.wait_for_message("/camera/color/image_raw", Image)
             print('waiting for goal image')
         goal_image = ros_numpy.numpify(raw_image)
-        goal_latent = self.get_visual_latent(goal_image)
-        return goal_latent
+        goal_latent, goal_mask = self.get_visual_latent(goal_image)
+        return goal_latent, goal_mask
 
     def get_visual_latent(self, image):
         # TODO crop to square
@@ -142,15 +144,57 @@ class VisualPushing:
 
             # cv2.imwrite(os.path.join(IMAGE_PATH, "goal{}_rec.png".format(self.step_)), np.array(goal_recon_pil))
         # embed();exit()
-        if self.step_ > 1:
+        if self.step_ >= 1:
             with torch.no_grad():
                 goal_recon_tensor = self.vae.decode(torch.tensor(self.goal_latent).to(dtype=torch.float32).to(self.device).unsqueeze(0))
                 goal_recon_pil = transforms.ToPILImage()(goal_recon_tensor.squeeze().cpu())
                 self.goal_publisher.publish(ros_numpy.msgify(Image,cv2.rotate(np.array(goal_recon_pil),cv2.cv2.ROTATE_90_CLOCKWISE) ,'mono8'))
-                # if self.step_ % 10 == 0:
-                #     cv2.imwrite(os.path.join(IMAGE_PATH, "goal{}_rec.png".format(self.step_)), np.array(goal_recon_pil))
-        # cv2.imwrite(os.path.join(IMAGE_PATH, "{:0>5d}_c.png".format(self.step_)), recon.squeeze().cpu().numpy())
-        return latent
+
+            # publish overall image here
+            green_mask = self.get_green_mask(size = 64, mask = mask)
+            red_goal = self.get_red_mask(size = 64, mask = self.goal_mask)
+            gripper_cross = self.get_gripper_cross(size = 64)
+
+            goal_object_overlay = cv2.addWeighted(green_mask, 0.5, red_goal, 0.5, 0.0)
+            goal_object_overlay = cv2.addWeighted(goal_object_overlay, 0.5, gripper_cross, 0.5, 0.0)
+
+            self.overall_publisher.publish(ros_numpy.msgify(Image,np.array(cv2.rotate(goal_object_overlay,cv2.cv2.ROTATE_90_CLOCKWISE)) ,'rgb8'))
+
+
+            assert self.goal_mask is not None
+
+
+        return latent, mask
+
+    def get_red_mask(self, size, mask):
+        full = np.zeros((size, size, 3), np.uint8)
+        full[:] = (255, 0, 0)
+        full_red = cv2.bitwise_and(full, full, mask = mask)
+        return full_red
+
+    def get_gripper_cross(self, size):
+        L = 0.63 # image length in real
+        x_start = 0.34
+        y_start = 0.44
+        full = np.zeros((size, size, 3), np.uint8)
+        full[:] = (0,0,255)
+        cross_x = int((self.gripper_pos[0] - x_start)/L*size)
+        cross_y = int((self.gripper_pos[1] - y_start)/L*size)
+        cross_posx = min(max(cross_y, 0), 63)
+        cross_posy = min(max(0, cross_x), 63)
+        mask_cross = np.zeros((size, size), np.uint8)
+        mask_cross[cross_posx, :] = 255
+        mask_cross[:, cross_posy] = 255
+
+        blue_cross = cv2.bitwise_and(full, full, mask = mask_cross)
+        return blue_cross
+
+
+    def get_green_mask(self, size, mask):
+        full = np.zeros((size, size, 3), np.uint8)
+        full[:] = (0, 255, 0)
+        full_green = cv2.bitwise_and(full, full, mask = mask)
+        return full_green
 
 
     def get_obs_dict(self):
@@ -161,7 +205,7 @@ class VisualPushing:
             raw_image = rospy.wait_for_message("/camera/color/image_raw", Image)
         object_image = ros_numpy.numpify(raw_image)
 
-        object_latent = self.get_visual_latent(object_image)
+        object_latent, _ = self.get_visual_latent(object_image)
         goal_latent = self.goal_latent
         # gripper pos
         obs["gripper_pos"] = self.gripper_pos.copy()
